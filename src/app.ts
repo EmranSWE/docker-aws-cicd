f9764d835eb9590959161abbc462b268c60d9865
EmranSWE <mdemran.swe@gmail.com>
2024-05-09 01:09:02 +0600

    [DONE]

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     errorMessages: [
       {
         path: req.originalUrl,
-        message: 'Hey Dev| Your api not found| Please check it again! Hello AWS 🚀🚀',
+        message: 'Hey Emran | Im running using cicd 🚀🚀',
       },
     ],
   });
